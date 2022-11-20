99ee28174a0259491d4aa54587e5e1ac9a454408
lilyrc1 <lilyrc.freelance@gmail.com>
2022-11-20 01:38:15 -0300

    margen de elementos mediante container

--- assets/js/scrip.js
+++ assets/js/scrip.js
@@ -4,3 +4,6 @@ btn.addEventListener("click", function(){
   alert("Tu mensaje se envió correctamente.")
 
 })
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
