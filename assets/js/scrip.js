7ebd6fcf49db94be45ac26ab4f3a74d0c4ca398d
lilyrc1 <lilyrc.freelance@gmail.com>
2022-11-20 23:02:07 -0300

    agrego popover síguenos en redes icono fb

--- assets/js/scrip.js
+++ assets/js/scrip.js
@@ -11,3 +11,6 @@ const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstra
 
 
 
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
