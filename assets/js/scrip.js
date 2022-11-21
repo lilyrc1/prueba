7a63fb150cfd364d97f58822fdd837583e38a561
lilyrc1 <lilyrc.freelance@gmail.com>
2022-11-20 21:31:19 -0300

    orden de código

--- assets/js/scrip.js
+++ assets/js/scrip.js
@@ -7,3 +7,7 @@ btn.addEventListener("click", function(){
 
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
+
+
